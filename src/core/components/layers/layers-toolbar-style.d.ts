e5ed88c086876c0a480795cfcea2143056d16488
Damon Ulmi <damon.ulmi@nrcan-rncan.gc.ca>
2026-09-18 23:40:16 +0000

    types updated

--- src/core/components/layers/layers-toolbar-style.d.ts
+++ src/core/components/layers/layers-toolbar-style.d.ts
@@ -1,10 +1,10 @@
 import type { Theme } from '@mui/material/styles';
 import type { SxStyles } from '@/ui/style/types';
 /**
- * Gets custom sx classes for the map.
+ * Gets custom sx classes for the layers toolbar component.
  *
- * @param theme - The MUI theme object
+ * @param theme - The theme object
  * @returns The sx classes object
  */
 export declare const getSxClasses: (theme: Theme) => SxStyles;
-//# sourceMappingURL=map-style.d.ts.map
+//# sourceMappingURL=layers-toolbar-style.d.ts.map
